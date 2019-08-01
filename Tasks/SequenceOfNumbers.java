aea987a9dfdbc8e8dda12f79573d2f4735d55479
Vitalli <vitaliimar@ukr.net>
2019-08-01 20:12:29 +0300

    added some task

---
+++ Tasks/SequenceOfNumbers.java
@@ -0,0 +1,34 @@
+package tests;
+
+public class SequenceOfNumbers {
+
+    // !!! TASK:  Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere. !!!
+
+    public static void main(String[] args) {
+        System.out.println(array123(new int[]{1, 1, 2, 3, 1}));
+        System.out.println(array123(new int[]{1, 1, 2, 4, 1}));
+        System.out.println(array123(new int[]{1, 1, 2, 1, 2, 3}));
+    }
+
+
+    public static boolean array123(int[] nums) {
+        int temp = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 1) {
+                temp = 2;
+                if (nums[i + 1] == temp) {
+                    temp = 3;
+                    if (nums[i + 2] == temp) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+
+    }
+
+
+}
