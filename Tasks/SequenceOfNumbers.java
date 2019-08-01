f81d0fdf8f43f7d2d78eb1621ba158f320b4a3b5
Vitalli <vitaliimar@ukr.net>
2019-08-01 20:26:55 +0300

    changes some task

--- Tasks/SequenceOfNumbers.java
+++ Tasks/SequenceOfNumbers.java
@@ -8,11 +8,14 @@ public class SequenceOfNumbers {
         System.out.println(array123(new int[]{1, 1, 2, 3, 1}));
         System.out.println(array123(new int[]{1, 1, 2, 4, 1}));
         System.out.println(array123(new int[]{1, 1, 2, 1, 2, 3}));
+        System.out.println(array123(new int[]{1, 2}));
     }
 
 
     public static boolean array123(int[] nums) {
         int temp = 0;
+
+        if (nums.length < 3) return false;
 
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == 1) {
